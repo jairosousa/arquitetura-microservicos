fa2fc2048c05793dd049ddc3d4d36cba78fe670e
Jairo <jaironsousa@gmail.com>
2021-06-13 17:31:05 -0300

    :construction: Criação Microserviço pagamento inicio.

---
+++ pagamento/src/test/java/com/jnsousa/pagamento/PagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnsousa.pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
